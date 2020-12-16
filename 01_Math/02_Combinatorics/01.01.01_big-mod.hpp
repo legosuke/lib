706b89b7e0a6cd155a73f82b3835a73de7a862f5
legosuke <pascalprime0521@gmail.com>
2020-12-16 23:42:55 +0000

    [fix]

--- 01_Math/02_Combinatorics/01.01.01_big-mod.hpp
+++ 01_Math/02_Combinatorics/01.01.01_big-mod.hpp
@@ -1,16 +1,23 @@
 #pragma once
 #include <cstdint>
 
-inline std::int64_t mod(std::int64_t a, std::int64_t MOD) {
-    return (a % MOD + MOD) % MOD;
+/**
+ * @brief 大きな mod 上の計算
+ * @note O(1)
+ */
+inline std::int64_t mod(std::int64_t a, std::int64_t m) {
+    return (a % m + m) % m;
 }
 
-inline std::int64_t mul(std::int64_t a, std::int64_t b, std::int64_t MOD) {
-    a = mod(a, MOD), b = mod(b, MOD);
+/**
+ * @note O(\log{m})
+ */
+inline std::int64_t mul(std::int64_t a, std::int64_t b, std::int64_t m) {
+    a = mod(a, m), b = mod(b, m);
     std::int64_t res = 0;
     while (b) {
-        if (b & 1) res = mod(res + a, MOD);
-        a = mod(a + a, MOD);
+        if (b & 1) res = mod(res + a, m);
+        a = mod(a + a, m);
         b >>= 1;
     }
     return res;
--- 01_Math/02_Combinatorics/01.03.01_mod-pow.hpp
+++ 01_Math/02_Combinatorics/01.03.01_mod-pow.hpp
@@ -1,12 +1,16 @@
 #pragma once
 #include <cstdint>
 
-std::int64_t mod_pow(std::int64_t a, std::int64_t n, std::int32_t MOD) {
-    a = (a % MOD + MOD) % MOD;
+/**
+ * @brief 累乗 : $a^n\pmod{m}$
+ * @note O(\log{n})
+ */
+std::int64_t mod_pow(std::int64_t a, std::int64_t n, std::int32_t m) {
+    a = (a % m + m) % m;
     std::int64_t res = 1;
     while (n) {
-        if (n & 1) (res *= a) %= MOD;
-        (a *= a) %= MOD;
+        if (n & 1) (res *= a) %= m;
+        (a *= a) %= m;
         n >>= 1;
     }
     return res;

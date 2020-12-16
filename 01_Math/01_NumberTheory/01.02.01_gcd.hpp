6b1ebcbf7d82ff3ef8c9c3dca36acb8cf915b04b
legosuke <pascalprime0521@gmail.com>
2020-12-16 14:53:26 +0000

    [add] gcd

---
+++ 01_Math/01_NumberTheory/01.02.01_gcd.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#include <cstdint>
+
+/**
+ * @brief 最大公約数
+ */
+std::uint64_t gcd(std::uint64_t a, std::uint64_t b) {
+    while (b) {
+        std::uint64_t c = a % b;
+        a = b;
+        b = c;
+    }
+    return a;
+}

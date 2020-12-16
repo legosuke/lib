e35ef5cbe9e3606d379a0b6c40b4593a854112ca
legosuke <pascalprime0521@gmail.com>
2020-12-16 15:43:54 +0000

    [add]

---
+++ test/01_Math/01_NumberTheory/01.03.01_AOJ-NTL-1-C.test.cpp
@@ -0,0 +1,15 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/6/NTL/1/NTL_1_C"
+#include "../../../template/template.hpp"
+#include "../../../01_Math/01_NumberTheory/01.03.01_lcm.hpp"
+
+int main() {
+    int n;
+    cin >> n;
+    int ans = 1;
+    for (int i = 0; i < n; ++i) {
+        int a;
+        cin >> a;
+        ans = lcm(ans, a);
+    }
+    cout << ans << endl;
+}

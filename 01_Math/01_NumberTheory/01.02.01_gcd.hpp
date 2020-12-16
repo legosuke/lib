e615c5b02ffcc48c86f3a88bf4810047528ad2ed
legosuke <pascalprime0521@gmail.com>
2020-12-16 15:38:33 +0000

    [fix] gcd

--- 01_Math/01_NumberTheory/01.02.01_gcd.hpp
+++ 01_Math/01_NumberTheory/01.02.01_gcd.hpp
@@ -1,15 +1,12 @@
 #pragma once
 #include <cstdint>
+#include <utility>
 
 /**
  * @brief 最大公約数
  * @note O(\min\log(a,b))
  */
 std::uint64_t gcd(std::uint64_t a, std::uint64_t b) {
-    while (b) {
-        std::uint64_t c = a % b;
-        a = b;
-        b = c;
-    }
+    while (b) std::swap(b, a %= b);
     return a;
 }

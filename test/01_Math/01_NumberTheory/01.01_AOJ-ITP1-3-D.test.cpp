43ea9a7529147d359eeab423b4110f4f4bd7da2d
legosuke <pascalprime0521@gmail.com>
2020-12-16 10:26:03 +0000

    [add] divisor.naive

---
+++ test/01_Math/01_NumberTheory/01.01_AOJ-ITP1-3-D.test.cpp
@@ -0,0 +1,13 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/lesson/2/ITP1/all/ITP1_3_D"
+#include "../../../template/template.hpp"
+#include "../../../01_Math/01_NumberTheory/01.01_divisor.naive.hpp"
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    int ans = 0;
+    for (int d : divisor(c)) if (a <= d && d <= b) {
+        ++ans;
+    }
+    cout << ans << endl;
+}

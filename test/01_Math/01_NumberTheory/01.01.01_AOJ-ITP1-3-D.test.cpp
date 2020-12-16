10e83077e4499b4ba30fb3e29d77a9ef4211c7b3
legosuke <pascalprime0521@gmail.com>
2020-12-16 14:41:27 +0000

    [fix] divisor.naive

--- test/01_Math/01_NumberTheory/01.01.01_AOJ-ITP1-3-D.test.cpp
+++ test/01_Math/01_NumberTheory/01.01.01_AOJ-ITP1-3-D.test.cpp
@@ -1,6 +1,6 @@
 #define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/lesson/2/ITP1/all/ITP1_3_D"
 #include "../../../template/template.hpp"
-#include "../../../01_Math/01_NumberTheory/01.01_divisor.naive.hpp"
+#include "../../../01_Math/01_NumberTheory/01.01.01_divisor.naive.hpp"
 
 int main() {
     int a, b, c;

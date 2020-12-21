a5278a1b6b19b0911fe0a3bd4bd2f3cc6751695e
legosuke <pascalprime0521@gmail.com>
2020-12-21 00:31:00 +0000

    [fix]

--- 01_Math/02_Combinatorics/01.01.01_big-mod.hpp
+++ 01_Math/02_Combinatorics/01.01.01_big-mod.hpp
@@ -12,7 +12,7 @@ inline std::uint64_t mod(std::int64_t a, std::uint64_t m) {
 /**
  * @note O(1)
  */
-inline std::uint64_t mul(std::int64_t a, std::int64_t b, std::int64_t m) {
+inline std::uint64_t mul(std::int64_t a, std::int64_t b, std::uint64_t m) {
     __uint128_t am = mod(a, m), bm = mod(b, m);
     return std::uint64_t(am * bm % m);
 }

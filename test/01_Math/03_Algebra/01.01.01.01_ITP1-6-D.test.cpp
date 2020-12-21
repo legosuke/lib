a35283f8ba2a8da36e0bb4d3b441d32c5e811f8c
legosuke <pascalprime0521@gmail.com>
2020-12-21 07:38:50 +0000

    [add] test

---
+++ test/01_Math/03_Algebra/01.01.01.01_ITP1-6-D.test.cpp
@@ -0,0 +1,21 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/lesson/2/ITP1/6/ITP1_6_D"
+#include "../../../template/template.hpp"
+#include "../../../01_Math/03_Algebra/01.01.01.01_matrix.vector.hpp"
+#include "../../../01_Math/03_Algebra/01.02.01.00_matrix-operation.hpp"
+
+signed main() {
+    int n, m;
+    cin >> n >> m;
+    Matrix<int> A(n, m);
+    vector<int> b(m);
+    for (int i = 0; i < n; ++i) for (int j = 0; j < m; ++j) {
+        cin >> A[i][j];
+    }
+    for (int i = 0; i < m; ++i) {
+        cin >> b[i];
+    }
+    auto ans = A * b;
+    for (int i = 0; i < n; ++i) {
+        cout << ans[i] << endl;
+    }
+}

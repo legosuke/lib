451b82cab94503d71836c96900c3101e30a741c1
legosuke <pascalprime0521@gmail.com>
2020-12-21 07:48:55 +0000

    [add] test

---
+++ test/01_Math/03_Algebra/01.01.01.01_ITP1-7-D.test.cpp
@@ -0,0 +1,20 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/lesson/2/ITP1/7/ITP1_7_D"
+#include "../../../template/template.hpp"
+#include "../../../01_Math/03_Algebra/01.01.01.01_matrix.vector.hpp"
+#include "../../../01_Math/03_Algebra/01.02.01.00_matrix-operation.hpp"
+
+signed main() {
+    int n, m, l;
+    cin >> n >> m >> l;
+    Matrix<int> A(n, m), B(m, l);
+    for (int i = 0; i < n; ++i) for (int j = 0; j < m; ++j) {
+        cin >> A[i][j];
+    }
+    for (int i = 0; i < m; ++i) for (int j = 0; j < l; ++j) {
+        cin >> B[i][j];
+    }
+    auto ans = A * B;
+    for (int i = 0; i < n; ++i) for (int j = 0; j < l; ++j) {
+        cout << ans[i][j] << endl;
+    }
+}

e641efad80bb9940b715c43b640790ede59a1fee
legosuke <pascalprime0521@gmail.com>
2020-12-23 01:54:48 +0000

    [fix]

--- 01_Math/03_Algebra/01.01.01.01_matrix.vector.hpp
+++ 01_Math/03_Algebra/01.01.01.01_matrix.vector.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <cstdint>
+#include <ostream>
 #include <vector>
 
 /**
@@ -14,8 +15,14 @@ protected:
 public:
     Matrix(std::uint32_t n, std::uint32_t m, T x = 0) : n(n), m(m), v(n, std::vector<T>(m, x)) {}
     void fill(T x) { v.clear(); v.resize(n, std::vector<T>(m, x)); }
-    std::uint32_t height() const { return n; }
-    std::uint32_t width() const { return m; }
+    constexpr std::uint32_t height() const { return n; }
+    constexpr std::uint32_t width() const { return m; }
     inline const std::vector<T>& operator [] (std::uint32_t i) const { return (v.at(i)); }
     inline std::vector<T>& operator [] (std::uint32_t i) { return (v.at(i)); }
+    friend std::ostream& operator << (std::ostream& os, const Matrix<T>& M) {
+        for (std::uint32_t i = 0; i < M.height(); ++i) for (std::uint32_t j = 0; j < M.width(); ++j) {
+            os << M[i][j] << " \n"[j + 1 == M.width()];
+        }
+        return os;
+    }
 };
--- 01_Math/03_Algebra/01.01.01.02_matrix.array.hpp
+++ 01_Math/03_Algebra/01.01.01.02_matrix.array.hpp
@@ -13,8 +13,14 @@ protected:
 public:
     Matrix(T x = 0) { fill(x); }
     void fill(T x) { v.fill(v.begin(), v.end(), x); }
-    std::uint32_t height() const { return n; }
-    std::uint32_t width() const { return m; }
+    constexpr std::uint32_t height() const { return n; }
+    constexpr std::uint32_t width() const { return m; }
     inline const std::array<T, m>& operator [] (std::uint32_t i) const { return (v.at(i)); }
     inline std::array<T, m>& operator [] (std::uint32_t i) { return (v.at(i)); }
+    friend std::ostream& operator << (std::ostream& os, const Matrix<T, m>& M) {
+        for (std::uint32_t i = 0; i < M.height(); ++i) for (std::uint32_t j = 0; j < M.width(); ++j) {
+            os << M[i][j] << " \n"[j + 1 == M.width()];
+        }
+        return os;
+    }
 };

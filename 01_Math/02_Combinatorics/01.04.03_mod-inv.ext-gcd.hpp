fe032de3debe68f87f4636f1b4328b7581c9314e
legosuke <pascalprime0521@gmail.com>
2020-12-26 06:12:40 +0000

    [add]

---
+++ 01_Math/02_Combinatorics/01.04.03_mod-inv.ext-gcd.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include <cassert>
+#include <cstdint>
+#include "../01_NumberTheory/01.04.01_ext-gcd.hpp"
+
+/**
+ * @brief 逆元 : $a^{-1}\bmod{m}$ (拡張ユークリッドの互助法)
+ * @note O(\log{m})
+ * @warning a と m は互いに素
+ */
+std::uint32_t mod_inv(std::int64_t a, std::uint32_t m) {
+    std::int_least64_t x, y;
+    auto g = ext_gcd(a, m, x, y);
+    assert(g == 1);
+    return (std::uint32_t)(x % m);
+}
---
+++ test/01_Math/02_Combinatorics/01.04.03_AOJ-DPL-5-B.test copy.cpp
@@ -0,0 +1,17 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/7/DPL/5/DPL_5_E"
+#include "../../../template/template.hpp"
+#include "../../../01_Math/02_Combinatorics/01.04.03_mod-inv.ext-gcd.hpp"
+
+signed main() {
+    int n, k; cin >> n >> k;
+    if (n > k) {
+        cout << 0 << endl;
+        return 0;
+    }
+    const int MOD = 1000000007;
+    int ans = 1;
+    for (int i = 1; i <= k; ++i) (ans *= i) %= MOD;
+    for (int i = 1; i <= n; ++i) (ans *= mod_inv(i, MOD)) %= MOD;
+    for (int i = 1; i <= k - n; ++i) (ans *= mod_inv(i, MOD)) %= MOD;
+    cout << ans << endl;
+} 

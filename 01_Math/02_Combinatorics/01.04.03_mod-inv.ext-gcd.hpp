2481f208b9557da1c5d6145267123d1608c5e585
legosuke <pascalprime0521@gmail.com>
2020-12-26 07:07:27 +0000

    [fix]

--- 01_Math/02_Combinatorics/01.04.03_mod-inv.ext-gcd.hpp
+++ 01_Math/02_Combinatorics/01.04.03_mod-inv.ext-gcd.hpp
@@ -2,6 +2,7 @@
 #include <cassert>
 #include <cstdint>
 #include "../01_NumberTheory/01.04.01_ext-gcd.hpp"
+#include "01.01_mod-operation.hpp"
 
 /**
  * @brief 逆元 : $a^{-1}\bmod{m}$ (拡張ユークリッドの互助法)
@@ -12,5 +13,5 @@ std::uint32_t mod_inv(std::int64_t a, std::uint32_t m) {
     std::int_least64_t x, y;
     auto g = ext_gcd(a, m, x, y);
     assert(g == 1);
-    return (std::uint32_t)(x % m);
+    return mod(x, m);
 }

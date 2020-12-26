02c555eedf0fdbb875fd947f80533a8fbeaae9bf
legosuke <pascalprime0521@gmail.com>
2020-12-26 04:41:38 +0000

    [add]

---
+++ 01_Math/02_Combinatorics/01.04.01_mod-inv.fermat.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include <cstdint>
+#include "01.03.01_mod-pow.hpp"
+
+/**
+ * @brief 逆元 : $a^{-1}\bmod{p}$
+ * @note O(\log{p})
+ * @warning p は素数
+ */
+std::uint64_t mod_inv(std::int64_t a, std::uint32_t p) {
+    return mod_pow(a, p - 2, p);
+}
---
+++ test/01_Math/02_Combinatorics/01.04.01_AOJ-DPL-5-B.test.cpp
@@ -0,0 +1,17 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/7/DPL/5/DPL_5_B"
+#include "../../../template/template.hpp"
+#include "../../../01_Math/02_Combinatorics/01.04.01_mod-inv.fermat.hpp"
+
+signed main() {
+    int n, k; cin >> n >> k;
+    if (n > k) {
+        cout << 0 << endl;
+        return 0;
+    }
+    const int MOD = 1000000007;
+    int ans = 1;
+    for (int i = 1; i <= k; ++i) (ans *= i) %= MOD;
+    for (int i = 1; i <= n; ++i) (ans *= mod_inv(i, MOD)) %= MOD;
+    for (int i = 1; i <= n - k; ++i) (ans *= mod_inv(i, MOD)) %= MOD;
+    cout << ans << endl;
+} 

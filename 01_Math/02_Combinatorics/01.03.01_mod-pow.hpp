2ebcc5231ce300f885693d39360b5a2694e645f3
legosuke <pascalprime0521@gmail.com>
2020-12-26 03:25:35 +0000

    [fix]

--- 01_Math/02_Combinatorics/01.03.01_mod-pow.hpp
+++ 01_Math/02_Combinatorics/01.03.01_mod-pow.hpp
@@ -5,9 +5,9 @@
  * @brief 累乗 : $a^n\bmod{m}$
  * @note O(\log{n})
  */
-std::int64_t mod_pow(std::int64_t a, std::int64_t n, std::uint32_t m) {
+std::uint64_t mod_pow(std::int64_t a, std::uint64_t n, std::uint32_t m) {
     a = (a % m + m) % m;
-    std::int64_t res = 1;
+    std::uint64_t res = 1;
     while (n) {
         if (n & 1) (res *= a) %= m;
         (a *= a) %= m;

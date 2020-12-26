5409c3eba3ee1124439ca4a764e6bba531190894
legosuke <pascalprime0521@gmail.com>
2020-12-26 06:12:25 +0000

    [fix]

--- 01_Math/02_Combinatorics/01.04.01_mod-inv.fermat.hpp
+++ 01_Math/02_Combinatorics/01.04.01_mod-inv.fermat.hpp
@@ -3,10 +3,10 @@
 #include "01.03.01_mod-pow.hpp"
 
 /**
- * @brief 逆元 : $a^{-1}\bmod{p}$
+ * @brief 逆元 : $a^{-1}\bmod{p}$ (フェルマーの小定理)
  * @note O(\log{p})
  * @warning p は素数
  */
-std::uint64_t mod_inv(std::int64_t a, std::uint32_t p) {
+std::uint32_t mod_inv(std::int64_t a, std::uint32_t p) {
     return mod_pow(a, p - 2, p);
 }

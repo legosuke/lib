6d2203dbe7ecc0002f852a875f20a373ebe7002f
legosuke <pascalprime0521@gmail.com>
2020-12-26 04:53:45 +0000

    [fix]

--- test/01_Math/02_Combinatorics/01.04.01_AOJ-DPL-5-B.test.cpp
+++ test/01_Math/02_Combinatorics/01.04.01_AOJ-DPL-5-B.test.cpp
@@ -1,4 +1,4 @@
-#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/7/DPL/5/DPL_5_B"
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/7/DPL/5/DPL_5_E"
 #include "../../../template/template.hpp"
 #include "../../../01_Math/02_Combinatorics/01.04.01_mod-inv.fermat.hpp"
 

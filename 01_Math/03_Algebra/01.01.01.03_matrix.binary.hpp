f0e33f9307d3118e376e6c389110641bfb626ff4
legosuke <pascalprime0521@gmail.com>
2020-12-26 07:38:43 +0000

    [fix]

---
+++ 01_Math/03_Algebra/01.01.01.03_matrix.binary.hpp
@@ -0,0 +1,28 @@
+#pragma once
+#include <array>
+#include <bitset>
+#include <cstdint>
+
+/**
+ * @brief 行列 (バイナリ)
+ */
+template <std::uint32_t n, std::uint32_t m>
+class Matrix {
+protected:
+    std::array<std::bitset<m>, n> v;
+
+public:
+    Matrix(bool x = 0) { fill(x); }
+    void fill(bool x) { for_each(v.begin(), v.end(), [&](std::bitset<m> vv) { if (x) vv.set(); else vv.reset(); }); }
+    constexpr std::uint32_t height() const { return n; }
+    constexpr std::uint32_t width() const { return m; }
+    inline const std::bitset<m>& operator [] (std::uint32_t i) const { return (v.at(i)); }
+    inline std::bitset<m>& operator [] (std::uint32_t i) { return (v.at(i)); }
+    friend std::ostream& operator << (std::ostream& os, const Matrix<n, m>& M) {
+        for (std::uint32_t i = 0; i < M.height(); ++i) for (std::uint32_t j = 0; j < M.width(); ++j) {
+            os << M[i][j] << " \n"[j + 1 == M.width()];
+        }
+        return os;
+    }
+    using value_type = bool;
+};

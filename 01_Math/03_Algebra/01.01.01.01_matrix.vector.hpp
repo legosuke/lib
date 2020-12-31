4d57ab42cd6b0ed0751d08e6e3007c945a9dc935
legosuke <pascalprime0521@gmail.com>
2020-12-31 13:30:45 +0000

    [fix]

--- 01_Math/03_Algebra/01.01.01.01_matrix.vector.hpp
+++ 01_Math/03_Algebra/01.01.01.01_matrix.vector.hpp
@@ -1,31 +1,168 @@
 #pragma once
+#include <cassert>
 #include <cstdint>
 #include <ostream>
 #include <vector>
-#include "01.01.00_matrix-base.hpp"
-#include "01.02.01.00_matrix-operation.hpp"
 
 /**
  * @brief 行列 (vector)
  */
 template <class T>
-class matrix_vector : matrix_base {
-protected:
-    std::uint32_t n, m;
-    std::vector<std::vector<T>> v;
-
+class matrix_vector {
 public:
-    matrix_vector(std::uint32_t n, std::uint32_t m, T x = 0) : n(n), m(m), v(n, std::vector<T>(m, x)) {}
-    void fill(T x) { v.clear(); v.resize(n, std::vector<T>(m, x)); }
-    std::uint32_t height() const { return n; }
-    std::uint32_t width() const { return m; }
-    inline const std::vector<T>& operator [] (std::uint32_t i) const { return (v.at(i)); }
-    inline std::vector<T>& operator [] (std::uint32_t i) { return (v.at(i)); }
-    friend std::ostream& operator << (std::ostream& os, const matrix_vector<T>& M) {
-        for (std::uint32_t i = 0; i < M.height(); ++i) for (std::uint32_t j = 0; j < M.width(); ++j) {
-            os << M[i][j] << " \n"[j + 1 == M.width()];
-        }
-        return os;
-    }
     using value_type = T;
+
+    matrix_vector() = default;
+    explicit matrix_vector(std::uint32_t n, std::uint32_t m, T x = T(0)) { init(n, m, x); }
+    explicit matrix_vector(const matrix_vector& A) {
+        init(A.height(), A.width());
+        for (int i = 0; i < A.height(); ++i) for (int j = 0; j < A.width(); ++j) {
+            _v[i][j] = A._v[i][j];
+        }
+    }
+
+    std::uint32_t height() const {
+        return _n;
+    }
+    std::uint32_t width() const {
+        return _m;
+    }
+    void init(std::uint32_t n, std::uint32_t m, T x = T(0)) {
+        _n = n; _m = m;
+        _v.clear(); _v.assign(_n, std::vector<T>(_m, x));
+    }
+    const std::vector<T>& operator [] (std::uint32_t i) const {
+        return (_v.at(i));
+    }
+    std::vector<T>& operator [] (std::uint32_t i) {
+        return (_v.at(i));
+    }
+
+protected:
+    std::uint32_t _n, _m;
+    std::vector<std::vector<T>> _v;
 };
+
+template <class T>
+matrix_vector<T>  operator + (const matrix_vector<T>& A, const T& x) {
+    matrix_vector<T> res(A.height(), A.width());
+    for (std::uint32_t i = 0; i < A.height(); ++i) for (std::uint32_t j = 0; j < A.width(); ++j) {
+        res[i][j] = A[i][j] + x;
+    }
+    return res;
+}
+
+template <class T>
+matrix_vector<T>  operator + (const T& x, const matrix_vector<T>& A) {
+    matrix_vector<T> res(A.height(), A.width());
+    for (std::uint32_t i = 0; i < A.height(); ++i) for (std::uint32_t j = 0; j < A.width(); ++j) {
+        res[i][j] = x + A[i][j];
+    }
+    return res;
+}
+
+template <class T>
+matrix_vector<T>  operator + (const matrix_vector<T>& A, const matrix_vector<T>& B) {
+    assert(A.height() == B.height() && A.width() == B.width());
+    matrix_vector<T> res(A.height(), A.width());
+    for (std::uint32_t i = 0; i < A.height(); ++i) for (std::uint32_t j = 0; j < A.width(); ++j) {
+        res[i][j] = A[i][j] + B[i][j];
+    }
+    return res;
+}
+
+template <class T>
+matrix_vector<T>  operator - (const matrix_vector<T>& A, const T& x) {
+    matrix_vector<T> res(A.height(), A.width());
+    for (std::uint32_t i = 0; i < A.height(); ++i) for (std::uint32_t j = 0; j < A.width(); ++j) {
+        res[i][j] = A[i][j] - x;
+    }
+    return res;
+}
+
+template <class T>
+matrix_vector<T>  operator - (const T& x, const matrix_vector<T>& A) {
+    matrix_vector<T> res(A.height(), A.width());
+    for (std::uint32_t i = 0; i < A.height(); ++i) for (std::uint32_t j = 0; j < A.width(); ++j) {
+        res[i][j] = x - A[i][j];
+    }
+    return res;
+}
+
+template <class T>
+matrix_vector<T>  operator - (const matrix_vector<T>& A, const matrix_vector<T>& B) {
+    assert(A.height() == B.height() && A.width() == B.width());
+    matrix_vector<T> res(A.height(), A.width());
+    for (std::uint32_t i = 0; i < A.height(); ++i) for (std::uint32_t j = 0; j < A.width(); ++j) {
+        res[i][j] = A[i][j] - B[i][j];
+    }
+    return res;
+}
+
+template <class T>
+matrix_vector<T>  operator * (const matrix_vector<T>& A, const T& x) {
+    matrix_vector<T> res(A.height(), A.width());
+    for (std::uint32_t i = 0; i < A.height(); ++i) for (std::uint32_t j = 0; j < A.width(); ++j) {
+        res[i][j] = A[i][j] * x;
+    }
+    return res;
+}
+
+template <class T>
+matrix_vector<T>  operator * (const T& x, const matrix_vector<T>& A) {
+    matrix_vector<T> res(A.height(), A.width());
+    for (std::uint32_t i = 0; i < A.height(); ++i) for (std::uint32_t j = 0; j < A.width(); ++j) {
+        res[i][j] = x * A[i][j];
+    }
+    return res;
+}
+
+template <class T>
+std::vector<T>  operator * (const matrix_vector<T>& A, const std::vector<T>& v) {
+    assert(A.width() == (std::uint32_t)v.size());
+    std::vector<T> u(A.height(), T(0));
+    for (std::uint32_t i = 0; i < A.height(); ++i) for (std::uint32_t j = 0; j < A.width(); ++j) {
+        u[i] = u[i] + A[i][j] * v[j];
+    }
+    return u;
+}
+
+template <class T>
+matrix_vector<T>  operator * (const matrix_vector<T>& A, const matrix_vector<T>& B) {
+    assert(A.height() == B.height() && A.width() == B.width());
+    matrix_vector<T> res(A.height(), B.width());
+    for (std::uint32_t i = 0; i < A.height(); ++i) for (std::uint32_t j = 0; j < B.width(); ++j) for (std::uint32_t k = 0; k < A.width(); ++k) {
+        res[i][j] = res[i][j] + A[i][k] * B[k][j];
+    }
+    return res;
+}
+
+template <class T>
+matrix_vector<T>  operator / (const matrix_vector<T>& A, const T& x) {
+    matrix_vector<T> res(A.height(), A.width());
+    for (std::uint32_t i = 0; i < A.height(); ++i) for (std::uint32_t j = 0; j < A.width(); ++j) {
+        res[i][j] = A[i][j] / x;
+    }
+    return res;
+}
+
+template <class T>
+matrix_vector<T> operator ^ (matrix_vector<T> A, std::uint64_t n) {
+    assert(A.height() == A.width());
+    matrix_vector<T> B(A.height(), A.width());
+    for (int i = 0; i < A.height(); ++i) B[i][i] = 1;
+    while (n) {
+        if (n & 1) B = B * A;
+        A = A * A;
+        n >>= 1;
+    }
+    return B;
+}
+
+template <class T>
+std::ostream& operator << (std::ostream& os, const matrix_vector<T>& A) {
+    for (std::uint32_t i = 0; i < A.height(); ++i) for (std::uint32_t j = 0; j < A.width(); ++j) {
+        os << A[i][j] << " \n"[j + 1 == A.width()];
+    }
+    return os;
+}
---
+++ docs/matrix_vector.md
@@ -0,0 +1,10 @@
+---
+title: 行列 (vector)
+documentation_of: ../01_Math/03_Algebra/01.01.01.01_matrix.vector copy.hpp
+---
+
+## `class T` の要件
+
+- `T(0)` が定義されている．
+- `op = +, -, *, /` について、`T op T` が定義されている．
+- `operator <<` が定義されている．

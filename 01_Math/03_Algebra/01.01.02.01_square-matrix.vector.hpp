dc603268ad80113134eb5e38fa4b4fecb416039a
legosuke <pascalprime0521@gmail.com>
2021-01-01 16:38:05 +0000

    [fix]

--- 01_Math/03_Algebra/01.01.02.01_square-matrix.vector.hpp
+++ 01_Math/03_Algebra/01.01.02.01_square-matrix.vector.hpp
@@ -8,5 +8,10 @@
 template <class T>
 class square_matrix_vector : public matrix_vector<T> {
 public:
-    square_matrix_vector(std::uint32_t n, T x = 0) : matrix_vector<T>(n, n, x) {}
+    square_matrix_vector() = default;
+    explicit square_matrix_vector(std::uint32_t n, T x = T(0)) : matrix_vector<T>(n, n, x) {}
+    // square_matrix_vector& operator = (const matrix_vector<T>& rhs) {
+    //     *this->_n = rhs._n; *this->_m = rhs._m; *this->_v = rhs._v;
+    //     return *this;
+    // }
 };

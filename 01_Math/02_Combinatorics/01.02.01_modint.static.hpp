643983242716722db17894052383d1a30385d3c0
legosuke <pascalprime0521@gmail.com>
2021-01-07 20:48:31 +0000

    [fix]

--- 01_Math/02_Combinatorics/01.02.01_modint.static.hpp
+++ 01_Math/02_Combinatorics/01.02.01_modint.static.hpp
@@ -1,8 +1,9 @@
 #pragma once
 #include <cassert>
 #include <cstdint>
-#include <ostream>
+#include <iostream>
 #include <type_traits>
+#include "01.01_mod-operation.hpp"
 #include "01.02.00_modint-base.hpp"
 #include "01.03.01_mod-pow.hpp"
 #include "01.04.03_mod-inv.ext-gcd.hpp"
@@ -19,7 +20,7 @@ public:
     template <typename Integer>
     static_modint(const Integer& v) : _v((v % MOD + MOD) % MOD) {}
 
-    std::uint32_t mod() const {
+    std::uint32_t get_mod() const {
         return MOD;
     }
     std::uint32_t val() const {
@@ -67,6 +68,11 @@ public:
     friend std::ostream& operator << (std::ostream& os, const static_modint<MOD>& rhs) {
         return os << rhs._v;
     };
+    friend std::istream& operator >> (std::istream& is, static_modint<MOD>& rhs) {
+        is >> rhs._v;
+        rhs._v = mod(rhs._v, MOD);
+        return is;
+    }
 
 protected:
     std::uint32_t _v;

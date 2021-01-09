100f765a6501602a3d31857f23bcf46777a3e49e
legosuke <pascalprime0521@gmail.com>
2021-01-09 23:36:40 +0000

    [add]

---
+++ 02_DataStructure/02_SegmentTree/00.01.03_monoid.sum.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include <algorithm>
+#include <limits>
+#include <type_traits>
+#include "00.00_monoid.base.hpp"
+
+/**
+ * @brief モノイド (sum)
+ */
+template <class T>
+class sum_monoid : public monoid_base {
+public:
+    using value_type = T;
+    const T op(T lhs, T rhs) const { return lhs + rhs; }
+    const T e() const { return T(0); }
+};
---
+++ test/02_DataStructure/02_SegmentTree/00.01.01_AOJ-DSL-2-A.test copy.cpp
@@ -0,0 +1,16 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/problems/DSL_2_B"
+#include "../../../template/template.hpp"
+#include "../../../02_DataStructure/02_SegmentTree/00.01.03_monoid.sum.hpp"
+#include "../../../02_DataStructure/02_SegmentTree/01_segment-tree.hpp"
+
+signed main() {
+    int n, q;
+    cin >> n >> q;
+    SegmentTree<sum_monoid<int>> st(n);
+    for (int i = 0; i < q; ++i) {
+        int com, x, y;
+        cin >> com >> x >> y;
+        if (com == 0) st.set(x - 1, y + st.get(x - 1));
+        else cout << st.query(x - 1, y) << endl;
+    }
+}

3f36e8dccc66d8ce13c98029fca76bb5a275ae03
legosuke <pascalprime0521@gmail.com>
2021-01-10 05:03:20 +0000

    [fix]

--- 02_DataStructure/02_SegmentTree/00.02.02_mapping.plus-plus.hpp
+++ 02_DataStructure/02_SegmentTree/00.02.02_mapping.plus-plus.hpp
@@ -1,10 +1,7 @@
 #pragma once
 #include <cstdint>
-#include <limits>
-#include <type_traits>
 #include "00.02.00_mapping.base.hpp"
-#include "00.01.01_monoid.min.hpp"
-#include "00.01.04_monoid.update.hpp"
+#include "00.01.03_monoid.plus.hpp"
 
 /**
  * @brief 写像 (plus-plus)
@@ -12,8 +9,8 @@
 template <class T>
 class plus_plus_mapping : public mapping_base {
 public:
-    using data_type = typename min_monoid<T>::value_type;
-    using lazy_type = typename update_monoid<T>::value_type;
+    using data_type = typename plus_monoid<T>::value_type;
+    using lazy_type = typename plus_monoid<T>::value_type;
     static data_type op(data_type x, lazy_type f, std::uint32_t length) {
         return x + f * length;
     }

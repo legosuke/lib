dab0a9be733a8c37644f59322e419d3af98b1d14
legosuke <pascalprime0521@gmail.com>
2021-01-10 05:14:13 +0000

    [add]

--- 02_DataStructure/02_SegmentTree/00.02.04_mapping.update-plus.hpp
+++ 02_DataStructure/02_SegmentTree/00.02.04_mapping.update-plus.hpp
@@ -2,20 +2,20 @@
 #include <cstdint>
 #include <type_traits>
 #include "00.02.00_mapping.base.hpp"
-#include "00.01.01_monoid.min.hpp"
 #include "00.01.03_monoid.plus.hpp"
+#include "00.01.04_monoid.update.hpp"
 
 /**
  * @brief 写像 (plus-min)
  */
 template <class T>
-class plus_min_mapping : public mapping_base {
+class update_plus_mapping : public mapping_base {
     static_assert(std::is_arithmetic<T>::value);
 
 public:
-    using data_type = typename min_monoid<T>::value_type;
-    using lazy_type = typename plus_monoid<T>::value_type;
+    using data_type = typename plus_monoid<T>::value_type;
+    using lazy_type = typename update_monoid<T>::value_type;
     static data_type op(data_type x, lazy_type f, std::uint32_t length) {
-        return x + f;
+        return f * length;
     }
 };

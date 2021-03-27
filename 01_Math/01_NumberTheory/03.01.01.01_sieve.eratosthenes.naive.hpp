764050ce207178718450ea51bd4ef5813835087f
legosuke <pascalprime0521@gmail.com>
2021-03-27 09:59:16 +0000

    [add]

---
+++ 01_Math/01_NumberTheory/03.01.01.01_sieve.eratosthenes.naive.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include <cstdint>
+#include <vector>
+
+/**
+ * @brief エラトステネスの篩 (素朴な手法)
+ * @note O(n\log{\log{n}})
+ */
+std::vector<bool> sieve(std::uint32_t n) {
+    std::vector<bool> isp(n, true);
+    isp[0] = isp[1] = false;
+    for (std::uint32_t i = 2; i * i < n; ++i) if (isp[i]) {
+        for (std::uint32_t j = i * i; j < n; j += i) {
+            isp[j] = false;
+        }
+    }
+    return isp;
+}

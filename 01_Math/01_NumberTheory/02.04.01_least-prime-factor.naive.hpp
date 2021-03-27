db8609a135c38c7035f8c37e8e985e9e5b232200
legosuke <pascalprime0521@gmail.com>
2021-03-27 09:32:42 +0000

    [add]

---
+++ 01_Math/01_NumberTheory/02.04.01_least-prime-factor.naive.hpp
@@ -0,0 +1,13 @@
+#include <cstdint>
+
+/**
+ * @brief 最小素因数 (素朴な手法)
+ * @note O(\sqrt{n})
+ */
+std::uint64_t least_prime_factor(std::uint64_t n) {
+    if (n <= 1) return n;
+    for (std::uint64_t p = 2; p * p <= n; ++p) {
+        if (n % p == 0) return p;
+    }
+    return n;
+}

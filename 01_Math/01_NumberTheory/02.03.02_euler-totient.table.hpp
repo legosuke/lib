cf77c5fdb13257646d29a9084c2118cd3e87bdf3
legosuke <pascalprime0521@gmail.com>
2021-03-28 12:56:05 +0000

    [add]

---
+++ 01_Math/01_NumberTheory/02.03.02_euler-totient.table.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include <cstdint>
+#include <numeric>
+#include <vector>
+
+/**
+ * @brief オイラーのトーシェント関数 (テーブル)
+ * @note O(n\log{\log{n}})
+ */
+std::vector<std::uint32_t> totient(std::uint32_t n) {
+    std::vector<std::uint32_t> phi(n);
+    std::iota(phi.begin(), phi.end(), 0);
+    for (std::uint32_t i = 2; i < n; ++i) {
+        if (phi[i] != i) continue;
+        for (std::uint32_t j = i; j < n; j += i) {
+            phi[j] -= phi[j] / i;
+        }
+    }
+    return phi;
+}

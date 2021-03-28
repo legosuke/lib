5a002292b8d208cbeeef63c7a61f855924660799
legosuke <pascalprime0521@gmail.com>
2021-03-28 14:41:27 +0000

    [add]

---
+++ 01_Math/02_Combinatorics/01.03.03.02_mod-pow.table.composite.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include <cstdint>
+#include <vector>
+#include "01.03.01_mod-pow.hpp"
+#include "../01_NumberTheory/02.04.02_least-prime-factor.sieve.hpp"
+
+/**
+ * @brief 累乗の列挙 ; $a^k\bmod{m}, 1≤a<n$
+ * @note O(n\log{k}/\log{n}+n)
+ */
+std::vector<std::uint32_t> mod_pow_table(std::uint32_t n, std::uint64_t k, std::uint64_t m) {
+    auto lpf = least_prime_factor(n);
+    std::vector<std::uint32_t> res(n);
+    res[0] = 0; res[1] = 1;
+    for (std::uint32_t i = 2; i < n; ++i) {
+        if (lpf[i] == i) res[i] = mod_pow(i, k, m);
+        else res[i] = res[lpf[i]] * res[i / lpf[i]] % m;
+    }
+    return res;
+}
---
+++ docs/mod-pow.table.composite.md
@@ -0,0 +1,8 @@
+---
+title: 累乗の列挙 $a^k\bmod{m}, 1≤a<n$
+documentation_of: ../01_Math/01.03.03.02_mod-pow.table.composite.hpp
+---
+
+## 参考
+
+https://37zigen.com/linear-sieve/

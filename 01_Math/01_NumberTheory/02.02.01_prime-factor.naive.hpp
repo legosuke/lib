3b49dff4164c6bba39a596d15dbb6af5fd2c2b69
legosuke <pascalprime0521@gmail.com>
2021-04-09 01:29:30 +0000

    [fix]

--- 01_Math/01_NumberTheory/02.02.01_prime-factor.naive.hpp
+++ 01_Math/01_NumberTheory/02.02.01_prime-factor.naive.hpp
@@ -5,11 +5,11 @@
  * @brief 素因数分解 (素朴な手法)
  * @note O(√n)
  */
-std::vector<std::pair<std::uint64_t, std::uint8_t>> prime_factor(std::uint64_t n) {
-    std::vector<std::pair<std::uint64_t, std::uint8_t>> pf;
+std::vector<std::pair<std::uint64_t, std::uint32_t>> prime_factor(std::uint64_t n) {
+    std::vector<std::pair<std::uint64_t, std::uint32_t>> pf;
     for (std::uint64_t p = 2; p * p <= n; ++p) {
         if (n % p != 0) continue;
-        std::uint8_t e = 0;
+        std::uint32_t e = 0;
         while (n % p == 0) {
             n /= p;
             ++e;
--- 01_Math/01_NumberTheory/02.02.02_prime-factor.sieve.hpp
+++ 01_Math/01_NumberTheory/02.02.02_prime-factor.sieve.hpp
@@ -9,11 +9,11 @@ std::vector<std::uint32_t> lpf = least_prime_factor(MAX_N + 1);
  * @brief 素因数分解 (LPFを利用した手法)
  * @note O(log(n))
  */
-std::vector<std::pair<std::uint32_t, std::uint8_t>> prime_factor(std::uint32_t n) {
-    std::vector<std::pair<std::uint32_t, std::uint8_t>> pf;
+std::vector<std::pair<std::uint32_t, std::uint32_t>> prime_factor(std::uint32_t n) {
+    std::vector<std::pair<std::uint32_t, std::uint32_t>> pf;
     while (n > 1) {
         std::uint32_t p = lpf[n];
-        std::uint8_t e = 0;
+        std::uint32_t e = 0;
         while (n % p == 0) {
             n /= p;
             ++e;

5898883b50c7ec0bb99e348babaa66a2c0f4f335
legosuke <pascalprime0521@gmail.com>
2021-04-14 02:40:45 +0000

    [fix]

--- 01_Math/02_Combinatorics/01.06.02_chinese-remainder-theorem.gauss.hpp
+++ 01_Math/02_Combinatorics/01.06.02_chinese-remainder-theorem.gauss.hpp
@@ -4,7 +4,6 @@
 #include <type_traits>
 #include <utility>
 #include <vector>
-#include "../01_NumberTheory/01.03.01_lcm.hpp"
 #include "01.01_mod-operation.hpp"
 #include "01.04.03_mod-inv.ext-gcd.hpp"
 #include "01.06.00_pre-chinese-remainder-theorem.hpp"
@@ -17,13 +16,11 @@ template <typename Integer>
 std::pair<Integer, Integer> chinese_remainder_theorem(std::vector<Integer> r, std::vector<Integer> m) {
     static_assert(std::is_integral<Integer>::value);
     if (!pre_chinese_remainder_theorem(r, m)) return std::make_pair(0, -1);
-    Integer x = 0, M = 1;
-    for (std::uint32_t i = 0; i < r.size(); ++i) {
-        M = lcm(M, m[i]);
-    }
+    __uint128_t x = 0, M = 1;
+    for (std::uint32_t i = 0; i < r.size(); ++i) M *= m[i];
     for (std::uint32_t i = 0; i < r.size(); ++i) {
         Integer Mi = M / m[i], li = mod_inv(Mi, m[i]);
-        (x += r[i] * li * Mi % M) %= M;
+        (x += r[i] * li % M * Mi % M) %= M;
     }
     return std::make_pair(mod(x, M), M);
 }

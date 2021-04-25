5430e960fb8ae7c187d7da349b93b88afbd260c4
legosuke <pascalprime0521@gmail.com>
2021-04-25 02:53:47 +0000

    [fix]

--- 01_Math/01_NumberTheory/02.01.05.02_is-prime.miller-rabin.deterministic.hpp
+++ 01_Math/01_NumberTheory/02.01.05.02_is-prime.miller-rabin.deterministic.hpp
@@ -6,14 +6,19 @@
 /**
  * @brief 素数判定 (ミラー・ラビン、決定的)
  * @note O(log³(n))
- * @warning n ≤ 2
+ * @warning n ≤ 2⁶⁴
+ */
+/**
+ * @brief 素数判定 (ミラー・ラビン、決定的)
+ * @note O(log³(n))
+ * @warning n ≤ 2⁶⁴
  */
 bool is_prime(std::uint64_t n) {
-    if (n == 2) return true;
-    if (n < 2 || !(n & 1)) return false;
+    if (n < 2 || !(n & 1)) return (n == 2);
     std::uint64_t d = n - 1;
     while (!(d & 1)) d >>= 1;
     for (auto a : std::vector<std::uint32_t>{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37}) {
+        if (a >= n) break;
         std::uint64_t t = d, y = mod_pow(a, t, n);
         while (t != n - 1 && y != 1 && y != n - 1) {
             y = mod_pow(y, 2, n);
--- 01_Math/01_NumberTheory/02.02.03_prime-factor.rho.hpp
+++ 01_Math/01_NumberTheory/02.02.03_prime-factor.rho.hpp
@@ -3,7 +3,7 @@
 #include <random>
 #include <vector>
 #include "01.02.01_gcd.hpp"
-#include "02.01.05_is-prime.miller-rabin.hpp"
+#include "02.01.05.02_is-prime.miller-rabin.deterministic.hpp"
 
 /**
  * @brief 素因数分解 (ロー法)

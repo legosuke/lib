c53a0eae6accc7774b96748d54efb13457d4893a
legosuke <pascalprime0521@gmail.com>
2021-04-25 02:25:03 +0000

    [fix]

--- 01_Math/01_NumberTheory/02.01.04_is-prime.strong-pseudoprime.hpp
+++ 01_Math/01_NumberTheory/02.01.04_is-prime.strong-pseudoprime.hpp
@@ -14,17 +14,18 @@ bool is_prime(std::uint64_t n, std::uint32_t k = 20) {
     for (std::uint32_t i = 0; i < k; ++i) {
         auto a = random_number(2ul, n);
         if (gcd(n, a) != 1) return false;
-        std::uint32_t s = 0, n1 = n - 1, d = n1;
+        std::uint64_t s = 0, n1 = n - 1, d = n1;
         while (!(d & 1)) {
             d >>= 1;
             ++s;
         }
         auto pw = mod_pow(a, d, n);
-        if (pw == 1 || pw == n1) return true;
-        for (std::uint32_t i = 1; i < s; ++i) {
+        bool is_composite = (pw != 1);
+        for (std::uint32_t i = 0; i < s; ++i) {
+            is_composite &= (pw != n1);
             pw = mul(pw, pw, n);
-            if (pw == n1) return true;
         }
+        if (is_composite) return false;
     }
-    return false;
+    return true;
 }

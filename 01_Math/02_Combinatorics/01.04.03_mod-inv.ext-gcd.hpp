08fca25f3f23c9bd49ac5e1f56c6f4a1bcec8b13
legosuke <pascalprime0521@gmail.com>
2021-04-26 03:24:47 +0000

    [fix]

--- 01_Math/02_Combinatorics/01.04.03_mod-inv.ext-gcd.hpp
+++ 01_Math/02_Combinatorics/01.04.03_mod-inv.ext-gcd.hpp
@@ -1,6 +1,4 @@
 #pragma once
-#include <cassert>
-#include <cstdint>
 #include "../01_NumberTheory/01.04.01_ext-gcd.hpp"
 #include "01.01_mod-operation.hpp"
 
@@ -9,8 +7,8 @@
  * @note O(log(m))
  * @warning a と m は互いに素
  */
-std::uint32_t mod_inv(std::int64_t a, std::uint32_t m) {
-    std::int_least64_t x, y;
+std::uint64_t mod_inv(std::int64_t a, std::uint64_t m) {
+    __int128_t x, y;
     auto g = ext_gcd(a, m, x, y);
     assert(g == 1);
     return mod(x, m);

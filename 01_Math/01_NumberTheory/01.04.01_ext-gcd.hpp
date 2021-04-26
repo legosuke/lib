9200cee665c7ca77731ff73dcfe66693cd6833ab
legosuke <pascalprime0521@gmail.com>
2021-04-26 03:20:27 +0000

    [fix]

--- 01_Math/01_NumberTheory/01.04.01_ext-gcd.hpp
+++ 01_Math/01_NumberTheory/01.04.01_ext-gcd.hpp
@@ -11,7 +11,7 @@ template <typename Integer1, typename Integer2, typename Integer3>
 Integer1 ext_gcd(Integer1 a, Integer2 b, Integer3& x, Integer3& y) {
     static_assert(std::is_integral<Integer1>::value);
     static_assert(std::is_integral<Integer2>::value);
-    static_assert(std::is_integral<Integer3>::value);
+    static_assert(std::is_integral<Integer3>::value || std::is_same<Integer3, __int128_t>::value);
     if (b == 0) { x = 1; y = 0; return a; }
     auto g = ext_gcd(b, a % b, y, x);
     y -= a / b * x;

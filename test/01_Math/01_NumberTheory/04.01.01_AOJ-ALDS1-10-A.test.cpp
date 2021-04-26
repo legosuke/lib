ef262fa58022f59974d381510ac2fa730ea07eab
legosuke <pascalprime0521@gmail.com>
2021-04-26 05:55:38 +0000

    [add]

---
+++ test/01_Math/01_NumberTheory/04.01.01_AOJ-ALDS1-10-A.test.cpp
@@ -0,0 +1,9 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/lesson/1/ALDS1/1/ALDS1_10_A"
+#include "../../../template/template.hpp"
+#include "../../../01_Math/01_NumberTheory/04.01.01_fibonacci-number.binet.hpp"
+
+signed main() {
+    int n;
+    cin >> n;
+    cout << fibonacci_number(n, 1000000000001) << endl;
+}
